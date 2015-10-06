0d4ec758ea3502a8d20eadef7335fa35227bb417
Alison Rose Haugh <alison@D-173-250-182-120.dhcp4.washington.edu>
2015-10-06 16:56:44 -0700

    Create Hello app.

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -10,14 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var mainText: UILabel!
+    
+    @IBAction func labelChanger(sender: UIButton)
+    {
+        let buttonText = "Go Dawgs"
+        mainText.text = buttonText
+        print("button text = \(buttonText)")
     }
 
 
